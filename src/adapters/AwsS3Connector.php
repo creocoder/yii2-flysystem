98b6ad0cdd2de9bf1bcf6918804f51ba0a112294
Alexander Kochetov <creocoder@gmail.com>
2015-01-18 17:29:22 +0300

    Added AwsS3Connector

---
+++ src/adapters/AwsS3Connector.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @link https://github.com/creocoder/yii2-flysystem
+ * @copyright Copyright (c) 2015 Alexander Kochetov
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace creocoder\flysystem\adapters;
+
+use Aws\S3\S3Client;
+use League\Flysystem\Adapter\AwsS3;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\base\Object;
+
+/**
+ * AwsS3Connector
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
+class AwsS3Connector extends Object implements ConnectorInterface
+{
+    /**
+     * @var string
+     */
+    public $key;
+    /**
+     * @var string
+     */
+    public $secret;
+    /**
+     * @var string
+     */
+    public $region;
+    /**
+     * @var string
+     */
+    public $baseUrl;
+    /**
+     * @var string
+     */
+    public $bucket;
+    /**
+     * @var string
+     */
+    public $prefix;
+    /**
+     * @var array
+     */
+    public $options = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if ($this->key === null) {
+            throw new InvalidConfigException('The "key" property must be set.');
+        }
+
+        if ($this->secret === null) {
+            throw new InvalidConfigException('The "secret" property must be set.');
+        }
+
+        if ($this->bucket === null) {
+            throw new InvalidConfigException('The "bucket" property must be set.');
+        }
+    }
+
+    /**
+     * Establish an adapter connection.
+     *
+     * @return AwsS3
+     */
+    public function connect()
+    {
+        $config = ['key' => $this->key, 'secret' => $this->secret];
+
+        if ($this->region !== null) {
+            $config['region'] = $this->region;
+        }
+
+        if ($this->baseUrl !== null) {
+            $config['base_url'] = $this->baseUrl;
+        }
+
+        return new AwsS3(S3Client::factory($config), $this->bucket, $this->prefix, $this->options);
+    }
+}
