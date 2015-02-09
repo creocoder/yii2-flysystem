03ffd55a67504e27d2834aad007daa15b86d2f46
Alexander Kochetov <creocoder@gmail.com>
2015-02-09 20:16:45 +0300

    Added ZipArchiveFilesystem

---
+++ src/ZipArchiveFilesystem.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @link https://github.com/creocoder/yii2-flysystem
+ * @copyright Copyright (c) 2015 Alexander Kochetov
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace creocoder\flysystem;
+
+use League\Flysystem\ZipArchive\ZipArchiveAdapter;
+use Yii;
+use yii\base\InvalidConfigException;
+
+/**
+ * ZipArchiveFilesystem
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
+class ZipArchiveFilesystem extends Filesystem
+{
+    /**
+     * @var string
+     */
+    public $path;
+    /**
+     * @var string|null
+     */
+    public $prefix;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if ($this->path === null) {
+            throw new InvalidConfigException('The "path" property must be set.');
+        }
+
+        $this->path = Yii::getAlias($this->path);
+    }
+
+    /**
+     * @return ZipArchiveAdapter
+     */
+    public function getAdapter()
+    {
+        return new ZipArchiveAdapter($this->path, null, $this->prefix);
+    }
+}
