be9454c86fa26f01cfe22f3d67cd75e911225520
Alexander Kochetov <creocoder@gmail.com>
2015-02-09 20:15:49 +0300

    Added CopyFilesystem

---
+++ src/CopyFilesystem.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @link https://github.com/creocoder/yii2-flysystem
+ * @copyright Copyright (c) 2015 Alexander Kochetov
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace creocoder\flysystem;
+
+use Barracuda\Copy\API;
+use League\Flysystem\Copy\CopyAdapter;
+use Yii;
+use yii\base\InvalidConfigException;
+
+/**
+ * CopyFilesystem
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
+class CopyFilesystem extends Filesystem
+{
+    /**
+     * @var string
+     */
+    public $consumerKey;
+    /**
+     * @var string
+     */
+    public $consumerSecret;
+    /**
+     * @var string
+     */
+    public $accessToken;
+    /**
+     * @var string
+     */
+    public $tokenSecret;
+    /**
+     * @var string|null
+     */
+    public $prefix;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if ($this->consumerKey === null) {
+            throw new InvalidConfigException('The "consumerKey" property must be set.');
+        }
+
+        if ($this->consumerSecret === null) {
+            throw new InvalidConfigException('The "consumerSecret" property must be set.');
+        }
+
+        if ($this->accessToken === null) {
+            throw new InvalidConfigException('The "accessToken" property must be set.');
+        }
+
+        if ($this->tokenSecret === null) {
+            throw new InvalidConfigException('The "tokenSecret" property must be set.');
+        }
+    }
+
+    /**
+     * @return CopyAdapter
+     */
+    public function getAdapter()
+    {
+        return new CopyAdapter(
+            new API($this->consumerKey, $this->consumerSecret, $this->accessToken, $this->tokenSecret),
+            $this->prefix
+        );
+    }
+}
