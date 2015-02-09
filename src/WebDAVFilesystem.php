7362bdb664e59b17def5ab3835cd22c647c72e46
Alexander Kochetov <creocoder@gmail.com>
2015-02-09 20:16:31 +0300

    Added WebDAVFilesystem

---
+++ src/WebDAVFilesystem.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @link https://github.com/creocoder/yii2-flysystem
+ * @copyright Copyright (c) 2015 Alexander Kochetov
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace creocoder\flysystem;
+
+use League\Flysystem\WebDAV\WebDAVAdapter;
+use Sabre\DAV\Client;
+use Yii;
+use yii\base\InvalidConfigException;
+
+/**
+ * WebDAVFilesystem
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
+class WebDAVFilesystem extends Filesystem
+{
+    /**
+     * @var string
+     */
+    public $baseUri;
+    /**
+     * @var string
+     */
+    public $userName;
+    /**
+     * @var string
+     */
+    public $password;
+    /**
+     * @var string
+     */
+    public $proxy;
+    /**
+     * @var integer
+     */
+    public $authType;
+    /**
+     * @var integer
+     */
+    public $encoding;
+    /**
+     * @var string|null
+     */
+    public $prefix;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if ($this->baseUri === null) {
+            throw new InvalidConfigException('The "baseUri" property must be set.');
+        }
+    }
+
+    /**
+     * @return WebDAVAdapter
+     */
+    public function getAdapter()
+    {
+        $config = [];
+
+        foreach ([
+            'baseUri',
+            'userName',
+            'password',
+            'proxy',
+            'authType',
+            'encoding',
+        ] as $name) {
+            if ($this->$name !== null) {
+                $config[$name] = $this->$name;
+            }
+        }
+
+        return new WebDAVAdapter(
+            new Client($config),
+            $this->prefix
+        );
+    }
+}
