d765d581f1cf844a964031e0f2121f7f5afeff13
Alexander Kochetov <creocoder@gmail.com>
2015-02-09 20:16:04 +0300

    Added DropboxFilesystem

---
+++ src/DropboxFilesystem.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @link https://github.com/creocoder/yii2-flysystem
+ * @copyright Copyright (c) 2015 Alexander Kochetov
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace creocoder\flysystem;
+
+use Dropbox\Client;
+use League\Flysystem\Dropbox\DropboxAdapter;
+use Yii;
+use yii\base\InvalidConfigException;
+
+/**
+ * DropboxFilesystem
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
+class DropboxFilesystem extends Filesystem
+{
+    /**
+     * @var string
+     */
+    public $token;
+    /**
+     * @var string
+     */
+    public $app;
+    /**
+     * @var string|null
+     */
+    public $prefix;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if ($this->token === null) {
+            throw new InvalidConfigException('The "token" property must be set.');
+        }
+
+        if ($this->app === null) {
+            throw new InvalidConfigException('The "app" property must be set.');
+        }
+    }
+
+    /**
+     * @return DropboxAdapter
+     */
+    public function getAdapter()
+    {
+        return new DropboxAdapter(new Client($this->token, $this->app), $this->prefix);
+    }
+}
