3c9489c03960a388c7f85db1ec0271567cfbda48
Alexander Kochetov <creocoder@gmail.com>
2015-02-15 14:33:38 +0300

    Added AzureFilesystem

---
+++ src/AzureFilesystem.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @link https://github.com/creocoder/yii2-flysystem
+ * @copyright Copyright (c) 2015 Alexander Kochetov
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace creocoder\flysystem;
+
+use League\Flysystem\Azure\AzureAdapter;
+use WindowsAzure\Common\ServicesBuilder;
+use Yii;
+use yii\base\InvalidConfigException;
+
+/**
+ * AzureFilesystem
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
+class AzureFilesystem extends Filesystem
+{
+    /**
+     * @var string
+     */
+    public $accountName;
+    /**
+     * @var string
+     */
+    public $accountKey;
+    /**
+     * @var string
+     */
+    public $container;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if ($this->accountName === null) {
+            throw new InvalidConfigException('The "accountName" property must be set.');
+        }
+
+        if ($this->accountKey === null) {
+            throw new InvalidConfigException('The "accountKey" property must be set.');
+        }
+
+        if ($this->container === null) {
+            throw new InvalidConfigException('The "container" property must be set.');
+        }
+
+        parent::init();
+    }
+
+    /**
+     * @return AzureAdapter
+     */
+    protected function prepareAdapter()
+    {
+        return new AzureAdapter(
+            ServicesBuilder::getInstance()->createBlobService(sprintf(
+                'DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s',
+                base64_encode($this->accountName),
+                base64_encode($this->accountKey)
+            )),
+            $this->container
+        );
+    }
+}
